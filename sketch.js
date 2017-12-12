564fbeaecaa4468437c6bf24fce114129b62b2ca
Steven Tonsil <32488902+StevenTonsil@users.noreply.github.com>
2017-12-12 09:14:42 -0500

    Updated Compatibility
    Controls are now synced correctly for mobile and desktop.

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let player;
 let enemies = [];
 let gravity = new p5.Vector(0, 0.3);
 let platforms = [];
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 //let testGround;
 
 function setup()
@@ -59,41 +60,99 @@ function draw()
 
 function keyPressed()
 {
-	// Left Arrow Key && Right Arrow Key
-	if (keyCode == 37 || keyCode == 39)
+	if (!isMobile)
 	{
-		player.toggleOn(keyCode);
-	}
-
-	// Up Arrow Key && Space Bar
-	if (((keyCode == 38 || keyCode == 32) && player.isOnGround) || !player.dJump && ((keyCode == 38 || keyCode == 32)))
-	{
-		if (!player.isOnGround)
+		// Left Arrow Key && Right Arrow Key
+		if (keyCode == 37 || keyCode == 39)
 		{
-			player.vel.set(player.vel.x, 0);
-			player.dJump = !player.dJump;
+			player.toggleOn(keyCode);
 		}
-		
-		player.applyForce([0, -10]);
-	}
 
-	// Down Arrow Key
-	/*if (keyCode == 40)
-	{
-		player.applyForce([0, 1]);
-	}*/
-	
-	// R Key
-	if (keyCode == 82)
-		resetMap();
+		// Up Arrow Key && Space Bar
+		if (((keyCode == 38 || keyCode == 32) && player.isOnGround) || !player.dJump && ((keyCode == 38 || keyCode == 32)))
+		{
+			if (!player.isOnGround)
+			{
+				player.vel.set(player.vel.x, 0);
+				player.dJump = !player.dJump;
+			}
+
+			player.applyForce([0, -10]);
+		}
+
+		// Down Arrow Key
+		/*if (keyCode == 40)
+		{
+			player.applyForce([0, 1]);
+		}*/
+
+		// R Key
+		if (keyCode == 82)
+			resetMap();
+	}
 }
 
 function keyReleased()
 {
-	// Left Arrow Key && Right Arrow Key
-	if (keyCode == 37 || keyCode == 39)
+	if (!isMobile)
 	{
-		player.toggleOff(keyCode);
+		// Left Arrow Key && Right Arrow Key
+		if (keyCode == 37 || keyCode == 39)
+		{
+			player.toggleOff(keyCode);
+		}
+	}
+}
+
+function mousePressed()
+{
+	if (isMobile)
+	{
+		// Left
+		if (mouseX < windowWidth/4)
+		{
+			player.toggleOn(37);
+		}
+		
+		// Right
+		else if (mouseX > 3 * windowWidth/4)
+		{
+			player.toggleOn(39);	
+		}
+
+		// Up
+		if (mouseY < windowHeight/4 && (player.isOnGround || !player.dJump))
+		{
+			if (!player.isOnGround)
+			{
+				player.vel.set(player.vel.x, 0);
+				player.dJump = !player.dJump;
+			}
+
+			player.applyForce([0, -10]);
+		}
+
+		// Bottom Middle
+		if ((mouseX > windowWidth/4 && mouseX < 3 * windowWidth/4) && mouseY > 3 * windowHeight/4)
+			resetMap();
+	}
+}
+
+function mouseReleased()
+{
+	if (isMobile)
+	{
+		// Left
+		if (mouseX < windowWidth/4)
+		{
+			player.toggleOff(37);
+		}
+		
+		// Right
+		else if (mouseX > 3 * windowWidth/4)
+		{
+			player.toggleOff(39);	
+		}
 	}
 }
 
